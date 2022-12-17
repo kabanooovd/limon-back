3db749f603fc92f3d6264997b9613c39f34fba48
dimas <kabanooovd@yandex.ru>
2022-12-18 02:53:31 +0300

    fiexd removement

--- src/utils/onRemoveAllFiles.ts
+++ src/utils/onRemoveAllFiles.ts
@@ -3,12 +3,15 @@ import path from "path";
 import { FILE_STORAGE_DIR_NAME } from "../config";
 
 export const onRemoveAllStaticFiles = () => {
-  fs.readdir(FILE_STORAGE_DIR_NAME, (err: any, files: any) => {
-    if (err) throw err;
-    for (const file of files) {
-      fs.unlink(path.join(FILE_STORAGE_DIR_NAME, file), (err: any) => {
-        if (err) throw err;
-      });
-    }
-  });
+  const staticPath = path.resolve(`${FILE_STORAGE_DIR_NAME}`)
+  if (fs.existsSync(staticPath)) {
+    fs.readdir(FILE_STORAGE_DIR_NAME, (err: any, files: any) => {
+      if (err) throw err;
+      for (const file of files) {
+        fs.unlink(path.join(FILE_STORAGE_DIR_NAME, file), (err: any) => {
+          if (err) throw err;
+        });
+      }
+    });
+  }
 }
--- src/utils/onRemoveFileByName.ts
+++ src/utils/onRemoveFileByName.ts
@@ -3,16 +3,17 @@ import path from "path";
 import { FILE_STORAGE_DIR_NAME } from "../config";
 
 export const onRemoveFileByName = (filename: string) => {
-  fs.readdir(FILE_STORAGE_DIR_NAME, (err: any, files: any) => {
-    if (err) throw err;
-    for (const file of files) {
-      if (file === filename) {
-        fs.unlink(path.join(FILE_STORAGE_DIR_NAME, file), (err: any) => {
-          if (err) throw err;
-        });
+  const staticPath = path.resolve(`${FILE_STORAGE_DIR_NAME}`)
+  if (fs.existsSync(staticPath)) {
+    fs.readdir(FILE_STORAGE_DIR_NAME, (err: any, files: any) => {
+      if (err) throw err;
+      for (const file of files) {
+        if (file === filename) {
+          fs.unlink(path.join(FILE_STORAGE_DIR_NAME, file), (err: any) => {
+            if (err) throw err;
+          });
+        }
       }
-    }
-
-  })
-  
+    })
+  }
 }
