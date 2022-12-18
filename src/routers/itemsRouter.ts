0988c40d2d28b470b365e4e1748452b467e72cde
dimas <kabanooovd@yandex.ru>
2022-12-18 13:54:14 +0300

    added swagger configuration to base host

--- src/routers/itemsRouter.ts
+++ src/routers/itemsRouter.ts
@@ -10,10 +10,96 @@ const itemsRouter = express.Router();
 
 const { items } = TABLES
 
+/**
+ * @swagger
+ * /api/items:
+ *  get:
+ *    description: Get all items data
+ *    responses:
+ *      200:
+ *        description: A successful response
+ */
 itemsRouter.get(`/${items}`, ItemsController.onGetItems)
+
+/**
+ * @swagger
+ *  /api/items/{id}:
+ *  get:
+ *    summary: Find item by id
+ *    description: Use to get item by id from query
+ *    parameters:
+ *      - in: path
+ *        name: id
+ *        required: true
+ *        description: uuid to get item.
+ *        schema:
+ *          type: string
+ *    responses:
+ *      200:
+ *        description: Item found successfully
+ */
 itemsRouter.get(`/${items}/:id`, ItemsController.onGetItemById)
+
+/**
+ * @swagger
+ *  /api/items:
+ *  post:
+ *    summary: Create new Item
+ *    description: Fill All requiered params to create new Item
+ *    parameters:
+ *      - in: formData
+ *        name: item_name
+ *        required: true
+ *        type: string
+ *        description: Insert Item title.
+ *      - in: formData
+ *        name: price
+ *        required: true
+ *        type: string
+ *        description: Insert Item price.
+ *      - in: formData
+ *        name: category
+ *        required: true
+ *        type: string
+ *        description: Insert Item category.
+ *      - in: formData
+ *        name: rating
+ *        required: true
+ *        type: string
+ *        description: Insert Item rating.
+ *      - in: formData
+ *        name: item_image
+ *        type: file
+ *        description: Insert Item Image.
+ *      - in: formData
+ *        name: item_description
+ *        type: string
+ *        description: Insert Item item_description.
+ *    responses:
+ *      201:
+ *        description: Item successfully created
+ */
 itemsRouter.post(`/${items}`, upload.single('item_image'), ItemsController.onPostItem)
+
 itemsRouter.delete(`/${items}/all`, ItemsController.onRemoveAllItems)
+
+/**
+ * @swagger
+ *  /api/items/{id}:
+ *  delete:
+ *    summary: Remove item by id
+ *    description: Use to remove item by id
+ *    parameters:
+ *      - in: path
+ *        name: id
+ *        required: true
+ *        description: uuid to remove item.
+ *        schema:
+ *          type: string
+ *    responses:
+ *      204:
+ *        description: A successful response
+ */
 itemsRouter.delete(`/${items}/:id`, ItemsController.onRemoveItemById)
 
 export default itemsRouter
